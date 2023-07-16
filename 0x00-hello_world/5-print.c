66963174e786bf7fc8591599872553f4e89b7fc8
murungaru001 <bazuusupreme@gmail.com>
2023-07-16 14:58:09 -0700

    print.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Code prints exactly a line with good grammar
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
