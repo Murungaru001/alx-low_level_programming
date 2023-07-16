5ef77e9214828d571099d71111aa1c5bbd7bfd59
murungaru001 <bazuusupreme@gmail.com>
2023-07-16 15:12:58 -0700

    printf.c

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,10 +1,2 @@
-#include <stdio.h>
-/**
- * main - Code programs  that prints a  line with printf
- * Return: 0
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
-}
+:wq
+
