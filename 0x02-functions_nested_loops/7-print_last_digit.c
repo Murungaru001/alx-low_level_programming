8353e9f59a13d9e0b5d5fa2e828b1dfbf5286ca9
murungaru001 <bazuusupreme@gmail.com>
2023-07-19 17:07:01 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int print_last_digit(void)
 {
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+int last;
+last = n %10;
+if (last < 0)
+{
+last = last * -1;
+}
+_putchar(last + '0');
+return (last);
 }
